af8c2be802562243da1c82f0760557cf0991aa9a
demil133 <demil133@gmail.com>
2013-10-20 01:47:01 +0200

    IMPROVE: add lots of coments and define routing information as class params

--- Smartphone.cc
+++ Smartphone.cc
@@ -7,34 +7,59 @@
 
 class Smartphone : public cSimpleModule
 {
-    protected:
-	cPar *intervalp;
-	cMessage *timerMsg;     // message for scheudling self calls
-	virtual CustomMessage *generateMessage();
-	virtual void initialize();
-	virtual void handleMessage(cMessage *msg);
-	virtual void forwardMessage(CustomMessage *msg);
+protected:
+    cPar *intervalp;
+    cMessage *timerMsg;     // message for scheudling self calls
+    virtual CustomMessage *generateMessage();
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+    virtual void forwardMessage(CustomMessage *msg);
 
-    public:
-	Smartphone();
-	virtual ~Smartphone();
+public:
+    Smartphone();
+    virtual ~Smartphone();
+
+private:
+    int srcDevice;
+    int srcNetwork;
+    int destDevice;
+    int destNetwork;
+    bool isUpstream;
 };
 
 Define_Module(Smartphone);
 
+/**
+ * Default constructor, does nothing but ensuring some params are null
+ */
 Smartphone::Smartphone(){
     timerMsg = NULL;
 }
 
+/**
+ * Default destructor. Does nothing but cancelling the scheudled packet forwards
+ */
 Smartphone::~Smartphone(){
     cancelAndDelete(timerMsg);
 }
 
+/**
+ * Called right after the class is instantiated. For some simulation reasons
+ * those tings should not be called at the constructor.
+ */
 void Smartphone::initialize()
 {
+    // kickstarts the event timing
     timerMsg = new cMessage("timer");
     intervalp = &par("interval");
     scheduleAt(simTime() + intervalp->doubleValue(), timerMsg);
+
+    // Since routing information will be static it can be initialized here
+    srcDevice = getIndex();
+    srcNetwork = NULL;
+    destDevice = 0;
+    destNetwork = 0;
+    isUpstream = true;
 }
 
 /**
@@ -43,34 +68,32 @@ void Smartphone::initialize()
  * one we sent has ben returned so we do nothing.
  */
 void Smartphone::handleMessage(cMessage *msg)
+{
     if(msg==timerMsg){
-	CustomMessage *newmsg = generateMessage(); // Generates a new message
-	send(newmsg, "gate"); // Sends the fresh generated message.
-	simtime_t delay = par("interval"); // Gets another delay time
-	scheduleAt(simTime() + intervalp->doubleValue(), timerMsg);  // Scheudles a new event.
+        // Sends a new message
+        CustomMessage *newmsg = generateMessage();
+        send(newmsg, "gate");
+
+        // Scheudles a new event
+        intervalp = &par("interval");
+        scheduleAt(simTime() + intervalp->doubleValue(), timerMsg);
     }
 }
 
 /**
- * sort of a constructor for the custom message class
+ * Sort of a factory for the custom message class. Uses static routing data to
+ * instantiate a new CustomMessage object and returns it.
  */
 CustomMessage *Smartphone::generateMessage()
 {
-    // get source and destination
-    int srcDevice = getIndex();
-    int srcNetwork = 1;
-    int destDevice = 0;
-    int destNetwork = 0;
-    bool isUpstream = true;
-
     // writes the actual message content
     char msgname[16];
-    sprintf(msgname, "upstream message", src, dest);
+    sprintf(msgname, "upstream message");
 
     // Create message object and set source and destination field.
     CustomMessage *msg = new CustomMessage(msgname);
     msg->setSourceIndex(srcDevice);
-    msg->setSourceNetwork(DestinationNetwork(srcNetwork);
+    msg->setSourceNetwork(DestinationNetwork);
     msg->setDestinationIndex(destDevice);
     msg->setDestinationNetwork(destNetwork);
     msg->setIsUpstream(isUpstream);
